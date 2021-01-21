6c2d9ca6373ebb1c9ee3c529a48236b39e701708
ericaf7923 <72775447+ericaf7923@users.noreply.github.com>
2021-01-21 12:10:38 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,12 @@
+#while True
+    #print("sound level :" +input.sound_level())
+    #if input.sound_level()>5
+            #lights.set_all(lights.rgb(255, 0, 255))
+    #else: 
+        #light.set_all(light.rgb(255, 0, 255))
+while True: 
+    print("light level: " + str(input.light_level()))
+    if input.light_level() >11: 
+        music.power_down.play_until_done()
+    else: 
+        music.stop_all_sounds()
